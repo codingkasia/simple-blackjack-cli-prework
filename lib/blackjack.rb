3b4b44488e9956f1ca4a27c4cc6abd62c4d05807
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-31 03:37:36 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,7 +22,7 @@ end
 def initial_round
   # code #initial_round here
   card_total = deal_card + deal_card
-  display_card_totals(card_total)
+  display_card_total(card_total)
   return card_total
 end
 
