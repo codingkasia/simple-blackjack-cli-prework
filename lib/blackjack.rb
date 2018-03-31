a796c605031f8135ae501beff91aecd9d64043c1
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-31 03:28:14 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,18 +50,7 @@ end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
-def runner
-  # code runner here
-  welcome  
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
-  end
-  end_game(card_total)
 
-end
-    
 def runner
   welcome
   initial_round
